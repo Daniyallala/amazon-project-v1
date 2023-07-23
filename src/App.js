31cea9df73120e318a78c171ab859b39a4d01e6d
Daniyal Lala <daniyallala18@gmail.com>
2023-07-23 19:01:48 +0530

    init

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Checkout from "./Checkout";
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <Router>
       <div className="App">
         <Switch>
           <Route path="/checkout">
-            <Header/>
-            <Checkout/>
+            <Header />
+            <Checkout />
           </Route>
           <Route path="/">
-            <Header/>
-            <Home/>
+            <Header />
+            <Home />
           </Route>
         </Switch>
       </div>
